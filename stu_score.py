e2c3ac3d98f853729c4fd616a804cec6ee31c931
clikks <anycosmo@live.com>
2017-04-05 23:01:19 +0800

    output score in file

--- stu_score.py
+++ stu_score.py
@@ -20,38 +20,61 @@ for k in all_report:
 
     for j in range(1,s):
         k[j] = float(k[j])
-        summit += k[j]
+    summit = sum(k[1:])
     k.append(summit)
     averge = summit / s
     k.append(averge)
 
 all_report = sorted(all_report,key=lambda d:d[-2],reverse=True)
-#计算每个学生的总成绩和平均分,并且根据分数高低排序
+    # cp_all_report = all_report[:]
 
-s = len(k)
-x = 2
-cp_all_report = all_report[:]
+
+
+#计算每个学生的总成绩和平均分,并且根据分数高低排序
+s = len(all_report[0])
+x = 1
+
+# print all_report
 
 while x < s:
     all = 0
-    for n in cp_all_report:
-        all += float(n[x])
-        if rank < len(all_report):
-            n.insert(0, rank)
+    for n in all_report:
+        all += n[x]
+        if rank <= len(all_report):
+            n.insert(0, str(rank))
             rank += 1
-    avg = all / len(cp_all_report)
+    avg = all / len(all_report)
     all_score.append('%.1f' %avg)
     x += 1
-
 #计算所有学生的每科平均分，总平均分,并添加名次
-print all_score
-print all_report
 
+# print all_score
+# print all_report
+title = ['名次','姓名','语文','数学','英语','物理','化学','生物','政治','历史','地理','总分','平均分']
 all_score.insert(0,'0\t平均')
 
-with open('./finnal_score.txt','w') as r:
-    title = '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\r' %('名次','姓名','语文','数学','英语','物理','化学','生物','政治','历史','地理','总分','平均分'
-    r.write(title)
+with open('finnal_score.txt','w') as f:
+    for i in title:
+        f.write(i)
+        f.write('\t')
+    f.write('\r')
     for i in all_score:
-        s = '%s\t' %i
-        r.write(s)
+        f.write(i)
+        f.write('\t')
+    f.write('\r')
+    for i in all_report:
+        # all_report_2 = [k = '不及格' for k in i[2:-2] if k < 60]
+        f.write(i[0])
+        f.write('\t')
+        f.write(str(i[1]))
+        f.write('\t')
+        for k in i[2:-2]:
+            if k < 60:
+                k = '不及格'
+            f.write(str(k))
+            f.write('\t')
+        f.write(str(i[-2]))
+        f.write('\t')
+        f.write(str(i[-1]))
+        f.write('\r')
+#处理学生不及格的成绩，并且将成绩格式化输出到文件中。
