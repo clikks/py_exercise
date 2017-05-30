608394daaa021fc76b348a243c64d42f3c8f4c88
turbotig <anycosmo@live.com>
2017-05-31 00:35:38 +0800

    bug fixing

--- medals.py
+++ medals.py
@@ -4,9 +4,6 @@ __author__ = 'clikks'
 
 import os
 
-print("=" * 24 + '\n' + " 奥运奖牌榜查询处理程序\n" + "=" * 24)
-#输出程序title
-
 def value(num,chose):
     while True:
         try:
@@ -18,52 +15,32 @@ def value(num,chose):
             chose = input("请使用数字选择:\n" + ">>> ")
 #判断选择操作有效性的函数，防止用户输入错误的类型
 
-if os.path.exists('./medals_tally.db'):
-    judge = True
-    chose = input("请选择您需要的操作:\n"
-                + "1) 查看奖牌榜\n"
-                + "2) 查看金牌榜\n"
-                + "3) 增加国家奖牌数量\n"
-                + "4) 退出程序\n"
-                + ">>> "
-                  )
-    value(4,chose)
-else:
-    judge = False
-    chose = input("暂无奖牌榜数据，您可执行以下操作:\n"
-          + "1) 增加国家奖牌数量\n"
-          + "2) 退出程序\n"
-          + ">>> "
-          )
-    value(2,chose)
-#判断总奖牌榜数据文件是否存在，存在则给出全部操作选项，否则只能新增奖牌
-
-
-
 class File_operate:
-    def __init__(self,dir ='./new_tally.db' ):
+    # def __init__(self,dir):
+    #     self.dir = dir
+
+    def openfile(self,dir):
         self.dir = dir
-
-    def openfile(self):
         global new
-        if chose == 1:
-            self.dir = './medals_tally.db'
-
-        with open(self.dir, 'r') as f:
+        with open(self.dir, 'w+') as f:
             new = {}
             for line in f.readlines():
                 line = line.strip()
                 text = line.split(':')[1].split(",")
-                text.pop()
+                # text.pop()
                 new[line.split(':')[0]] = text
 
-    def writefile(self,dir = './new_tally.db'):
-        self.dir = dir()
-        with open(self.new_tally,'w+') as f:
-            for i in new:
+    def writefile(self,dir,dict):
+        self.dir = dir
+        self.dict = dict
+        # self.dict2 = dict2
+        with open(self.dir,'w+') as f:
+            for i in self.dict:
                 f.write(i + ":")
-                for s in new[i]:
-                    f.write(s + ",")
+                for s in self.dict[i][0:-1]:
+                    f.write(str(s) + ",")
+                s = self.dict[i][-1]
+                f.write(str(s))
                 f.write('\r')
 
 class Medal_tally:
@@ -89,10 +66,65 @@ class Medal_tally:
         print(new)
     # 定义新增奖牌数方法,将新增奖牌的国家名和新增数写入new字典
 
-    # def sum_tally(self):
+    def sum_new_tally(self):
+        global all_tally
+        if judge == False:
+            File_operate().writefile('./medals_tally.db',new)
+            File_operate().writefile('./new_tally.db',new)
+        else:
+            all_tally = {}
+            with open('./medals_tally.db','r') as f:
+                for data in f.readlines():
+                    data = data.strip()
+                    print(data)
+                    doc = data.split(':')[1].split(",")
+                    print(doc)
+                    all_tally[data.split(':')[0]] = doc
+                print(all_tally)
+                for i in range(3):
+                    all_tally[self.name][i] = int(all_tally[self.name][i])
+                    new[self.name][i] = int(new[self.name][i])
+                    all_tally[self.name][i] += new[self.name][i]
+                # all_tally[self.name][1] += new[self.name][1
+                    print(all_tally)
 
-C1 = File_operate()
-C1.openfile()
+
+print("=" * 24 + '\n' + " 奥运奖牌榜查询处理程序\n" + "=" * 24)
+#输出程序title
+
+if os.path.exists('./medals_tally.db'):
+    judge = True
+    chose = input("请选择您需要的操作:\n"
+                + "1) 查看奖牌榜\n"
+                + "2) 查看金牌榜\n"
+                + "3) 增加国家奖牌数量\n"
+                + "4) 清除奖牌数据库\n"
+                + "5) 退出程序\n"
+                + ">>> "
+                  )
+    value(4,chose)
+    if chose == 3:
+        C1 = File_operate()
+        C1.openfile('./new_tally.db')
+    else:
+        C1 = File_operate()
+        C1.openfile('./medals_tally.db')
+else:
+    judge = False
+    chose = input("暂无奖牌榜数据，您可执行以下操作:\n"
+          + "1) 增加国家奖牌数量\n"
+          + "2) 退出程序\n"
+          + ">>> "
+          )
+    value(2,chose)
+    C1 = File_operate()
+    C1.openfile('./new_tally.db')
+#判断总奖牌榜数据文件是否存在，存在则给出全部操作选项，否则只能新增奖牌
+
+# C1 = File_operate()
+# print(new)
 C2 = Medal_tally()
 C2.new_tally()
-C1.writefile()
+C2.sum_new_tally()
+if judge:
+    C1.writefile('./medals_tally.db',all_tally)
