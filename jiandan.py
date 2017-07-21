522e4a9fd89acbb553912612dbf145e632bb02d3
clikks <anycosmo@live.com>
2017-07-22 01:00:42 +0800

    continue

--- jiandan.py
+++ jiandan.py
@@ -2,18 +2,53 @@
 # _*_ coding:utf-8 _*_
 __author__ = 'lux'
 
-import urllib.request
-import urllib.parse
-import tkinter as tk
-# start = int(input("请输入起始页："))
-# end = int(input("请输入终止页："))
-# url = 'http://jandan.net/ooxx/page-%d#comments'
-# print(url %start)
+import requests
+import re
+from bs4 import BeautifulSoup
 
 class Beauty:
-    def __init__(self,start,end,url):
+    def __init__(self, start=0, end=0):
         self.start = int(input("请输入起始页："))
-        self.end = end = int(input("请输入终止页："))
-        self.url = 'http://jandan.net/ooxx/page-%d#comments'
+        self.end = int(input("请输入终止页："))
+        # self.url = 'http://jandan.net/ooxx/page-%d#comments'
 
-    # def
+    def pic_url(self):
+        page = 'http://jandan.net/ooxx/page-%d#comments' %self.start
+        Url = requests.get(page)
+        # print(page)
+        Html = BeautifulSoup(Url.text,'lxml')
+        Img_url = Html.find_all('a',class_ = 'view_img_link')
+        # print(Img_url)
+        image = []
+        for i in Img_url:
+            i = str(i)
+            result = re.findall(r'href="//(\w+\d.\w+.\w+/\w+/\w+\.(jpg|gif|png|bmp))',i)
+            image.append(result[0][0])
+            print(image)
+
+    def download(self):
+
+HtmlOpen = requests.get('http://jandan.net/ooxx')
+# HtmlOpen.encoding = 'utf-8'
+# with open('jiandan.html','w',encoding='utf-8') as f:
+#     f.write(HtmlOpen.text)
+# Soup = BeautifulSoup(open('jiandan.html',encoding='utf-8'),'lxml')
+Soup = BeautifulSoup(HtmlOpen.text,'lxml')
+tag = Soup.find_all('span',class_='current-comment-page')
+page = str(tag[0].contents[0])
+comment_page = int(re.findall(r'\w+',page)[0])
+# print(comment_page)
+
+C1 = Beauty()
+C1.pic_url()
+
+
+
+
+# top = tkinter.Tk()
+# top.title('煎蛋妹子图下载器')
+# top.geometry('400x200')
+# def pageButton():
+#     print('目录')
+# button = tkinter.Button(top,text = '下载路径',command = pageButton).pack()
+# top.mainloop()
