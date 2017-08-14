2f705a3d36d791a51bcd5f3d8270e5395c6081d3
clikks <anycosmo@live.com>
2017-08-14 23:24:13 +0800

    download pic

--- jiandan.py
+++ jiandan.py
@@ -2,46 +2,54 @@
 # _*_ coding:utf-8 _*_
 __author__ = 'lux'
 
-import requests
-import re
 from bs4 import BeautifulSoup
+import urllib.request
+import re
+
 
 class Beauty:
-    def __init__(self, start=0, end=0):
+    def __init__(self, start=0, end=0,image=[]):
         self.start = int(input("请输入起始页："))
         self.end = int(input("请输入终止页："))
+        self.image = image
         # self.url = 'http://jandan.net/ooxx/page-%d#comments'
 
     def pic_url(self):
-        page = 'http://jandan.net/ooxx/page-%d#comments' %self.start
-        Url = requests.get(page)
-        # print(page)
-        Html = BeautifulSoup(Url.text,'lxml')
-        Img_url = Html.find_all('a',class_ = 'view_img_link')
-        # print(Img_url)
-        image = []
-        for i in Img_url:
-            i = str(i)
-            result = re.findall(r'href="//(\w+\d.\w+.\w+/\w+/\w+\.(jpg|gif|png|bmp))',i)
-            image.append(result[0][0])
-            print(image)
+        for input_pages in range(self.start,self.end+1):
+            pages = 'http://jandan.net/ooxx/page-%d#comments' %input_pages
+            Url = urllib.request.urlopen(pages)
+            # print(page)
+            Html = BeautifulSoup(Url.read(),'lxml')
+            Img_url = Html.find_all('a',class_ = 'view_img_link')
+            # print(Img_url)
+            for i in Img_url:
+                i = str(i)
+                result = re.findall(r'href="//(\w+\d.\w+.\w+/\w+/\w+\.(jpg|gif|png|bmp))',i)
+                self.image.append(result[0][0])
+                # print(image)
 
     def download(self):
+        # print(self.image)
+        # pattern = r'\w+\.(jpg|gif|png|bmp)'
+        for pic in self.image:
+            print(type(pic))
+            img_name = re.findall(r'(\w+\.(jpg|gif|png|bmp))',pic)[0][0]
+            pic_url = "http://" + pic
+            img_page = urllib.request.urlopen(pic_url,timeout=10).read()
+            with open(img_name,'wb') as f:
+                f.write(img_page)
 
-HtmlOpen = requests.get('http://jandan.net/ooxx')
-# HtmlOpen.encoding = 'utf-8'
-# with open('jiandan.html','w',encoding='utf-8') as f:
-#     f.write(HtmlOpen.text)
-# Soup = BeautifulSoup(open('jiandan.html',encoding='utf-8'),'lxml')
-Soup = BeautifulSoup(HtmlOpen.text,'lxml')
+HtmlOpen = urllib.request.urlopen('http://jandan.net/ooxx')
+Soup = BeautifulSoup(HtmlOpen.read(),'lxml')
 tag = Soup.find_all('span',class_='current-comment-page')
+print(tag)
 page = str(tag[0].contents[0])
 comment_page = int(re.findall(r'\w+',page)[0])
-# print(comment_page)
+print(comment_page)
 
 C1 = Beauty()
 C1.pic_url()
-
+C1.download()
 
 
 
